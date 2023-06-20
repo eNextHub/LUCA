7d688fa0860df4806233f19d35bdb446898424fd
Nicolò Golinucci <nicolo.golinucci@gmail.com>
2023-06-20 16:50:26 +0200

    Adding setup.py file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='LUCA',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'pint',
+    ],
+    entry_points={
+        'console_scripts': [
+            'fuel_context=LUCA.fuel_context:main',
+        ],
+    },
+    author='Nicolò Golinucci',
+    author_email='nicolo.golinucci@example.com',
+    description='Repository to create a pint-importabile historical memory of prices, heat powers, density of commodities.',
+    url='https://github.com/eNextHub/LUCA',
+)
