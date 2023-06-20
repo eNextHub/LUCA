f5eb61e1677b673922d1b58225fa08ff6cdc040f
Nicolò Golinucci <nicolo.golinucci@gmail.com>
2023-06-20 16:36:50 +0200

    Fixing the tab issue and intializing also a currecny unit (EUR)

--- Main.py
+++ Main.py
@@ -19,7 +19,7 @@ def fuel_context(path='Input/LUCA Table.xlsx', save='Output/LUCA for pint.txt'):
     df = pd.read_excel(file_name, header=[0,1,2], index_col=[0,1,2])
 
     sN = slice(None)
-    contexts = []
+    contexts = ["EUR = [currency] = euro"] # The currency must be added manually
 
     for c in range(len(df.columns)):
         name = df.columns.get_level_values(0)[c]
@@ -27,20 +27,20 @@ def fuel_context(path='Input/LUCA Table.xlsx', save='Output/LUCA for pint.txt'):
         code = df.columns.get_level_values(2)[c]
 
         context = f"""
-    # {name}
-    LHV_{code} = {df.loc[(sN,'LHV'),name].values[0][0]} {df.loc[(sN,'LHV'),name].index.get_level_values(2)[0]}
-    den_{code} = {df.loc[(sN,'den'),name].values[0][0]} {df.loc[(sN,'den'),name].index.get_level_values(2)[0]}
-    prV_{code} = {df.loc[(sN,'prV'),name].values[0][0]} {df.loc[(sN,'prV'),name].index.get_level_values(2)[0]}
-    prM_{code} = {df.loc[(sN,'prM'),name].values[0][0]} {df.loc[(sN,'prM'),name].index.get_level_values(2)[0]}
+# {name}
+LHV_{code} = {df.loc[(sN,'LHV'),name].values[0][0]} {df.loc[(sN,'LHV'),name].index.get_level_values(2)[0]}
+den_{code} = {df.loc[(sN,'den'),name].values[0][0]} {df.loc[(sN,'den'),name].index.get_level_values(2)[0]}
+prV_{code} = {df.loc[(sN,'prV'),name].values[0][0]} {df.loc[(sN,'prV'),name].index.get_level_values(2)[0]}
+prM_{code} = {df.loc[(sN,'prM'),name].values[0][0]} {df.loc[(sN,'prM'),name].index.get_level_values(2)[0]}
 
-    @context {cont} = {code}
-        [mass] -> [energy]: value * LHV_{code}
-        [energy] -> [mass]: value / LHV_{code}
-        [volume] -> [mass]: value * den_{code}
-        [mass] -> [volume]: value / den_{code}
-        [volume] -> [currency]: value * prV_{code}
-        [currency] -> [volume]: value / prV_{code}
-    @end
+@context {cont} = {code}
+    [mass] -> [energy]: value * LHV_{code}
+    [energy] -> [mass]: value / LHV_{code}
+    [volume] -> [mass]: value * den_{code}
+    [mass] -> [volume]: value / den_{code}
+    [volume] -> [currency]: value * prV_{code}
+    [currency] -> [volume]: value / prV_{code}
+@end
         """
         contexts.append(context)
 
